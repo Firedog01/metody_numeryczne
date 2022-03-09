58795dd53d25257fedf3e4c6b50c718de3dee483
236496 Baraniecki Krystian <236496@edu.p.lodz.pl>
2022-03-09 15:14:42 +0100

    Regex 3.0 wip 3

--- zad1/Function.py
+++ zad1/Function.py
@@ -2,80 +2,91 @@ import re
 import numpy as np
 
 
+def make_list(s):
+    s = re.sub(' ', '', s)
+    s = re.sub(r'\+', ' ', s)
+    s = re.sub('-', ' -', s)
+    s = re.sub(r'\( -', '(-', s)
+    s = re.sub('-x', '-1x', s)
+    s = re.sub(r'[^\d|^(]x', ' 1x', s)
+    fn_list = list(str.split(s))
+
+    # Function could have no '1' or '-1' at the beginning
+    for fn in fn_list:
+        print()
+        if re.search(r'^-[^\d]', fn):
+            fn = re.sub('^-', '-1', fn)
+        elif re.search('^[a-z]', fn):
+            fn = '1' + fn
+
+    return fn_list
+
+
+def make_lambda(fn):
+
+    # Trigonometry
+    if re.search('sin', fn):
+        # print("sinus: ", fn)
+        a = re.sub(r'sin\(x\)', '', fn)
+        return lambda x: float(a) * np.sin(x)
+    elif re.search('cos', fn):
+        # print("cosinus: ", fn)
+        a = re.sub(r'cos\(x\)', '', fn)
+        return lambda x: float(a) * np.cos(x)
+    elif re.search('tan', fn):
+        # print("tangens: ", fn)
+        a = re.sub(r'tan\(x\)', '', fn)
+        return lambda x: float(a) * np.tan(x)
+
+    # e^x
+    elif re.search(r'e\^x', fn):
+        # print("e do x:", fn)
+        a = re.sub(r'e\^x', '', fn)
+        return lambda x: float(a) * np.exp(x)
+
+    # Constant
+    elif not re.search(r'x', fn):
+        # print("stala: ", fn)
+        return lambda x: float(fn)
+
+    # ax^p
+    elif re.search(r"x\^?\d+?|(\(-\d+\))?$", fn):
+        print("jedn: ", fn)
+        a = re.sub('x', '', fn)
+        p = a
+        a = re.sub(r'\^-?(\d+|\(-?\d+\))$', '', a)
+        print("mian: ", a)
+        p = re.sub(a, '', p)
+        p = re.sub(r'\^\(?', '', p)
+        p = re.sub(r'\)?', '', p)
+        if p == '':
+            p = '1'
+        print("pow:", p)
+        print(a, "x", p)
+        return lambda x: float(a) * (float(x) ** float(p))
+
+
+def is_polymonial(fn_list: list):
+    for fn in fn_list:
+        if not re.match(r"\^\d+", fn) and not re.match(r"x", fn):
+            return False
+    return True
+
+
 class Function:
-    factors = list()
     funcs = list()
 
+    # Divide string into list and transform into lambdas
     def __init__(self, s: str):
-
-        s = re.sub(' ', '', s)
-        s = re.sub('\+', ' ', s)
-        s = re.sub('-', ' -', s)
-        s = re.sub('\( -', '(-', s)
-        s = re.sub('-x', '-1x', s)
-        s = re.sub('[^\d|^(]x', ' 1x', s)
-
-        for fn in list(str.split(s)):
-            # Function could have no '1' or '-1' at the beginning
-            if re.search('^-[^\d]', fn):
-                fn = re.sub('^-', '-1', fn)
-                print("a")
-            elif re.search('^[a-z]', fn):
-                fn = '1' + fn
-                print("b")
-
-            # Trigonometry
-            if re.search('sin', fn):
-                print("sinus: ", fn)
-                a = re.sub('sin\(x\)', '', fn)
-                self.funcs.append(lambda x: float(a) * np.sin(x))
-            elif re.search('cos', fn):
-                print("cosinus: ", fn)
-                a = re.sub('cos\(x\)', '', fn)
-                self.funcs.append(lambda x: float(a) * np.cos(x))
-            elif re.search('tan', fn):
-                print("tangens: ", fn)
-                a = re.sub('tan\(x\)', '', fn)
-                self.funcs.append(lambda x: float(a) * np.tan(x))
-
-            # e^x
-            elif re.search('e\^x', fn):
-                print("e do x:", fn)
-                a = re.sub('tan\(x\)', '', fn)
-                self.funcs.append(lambda x: float(a) * np.exp(x))
-
-            # ax^p
-            elif re.search('x\^?\d+?|(\(-\d+\))?$', fn):
-                print("jedn: ", fn)
-                a = re.sub('x', '', fn)
-                p = a
-                a = re.sub('\^-?(\d+|\(-?\d+\))$', '', a)
-                print("mian: ", fn)
-                p = re.sub(fn, '', p)
-                p = re.sub('\^\(?', '', p)
-                p = re.sub('\)?', '', p)
-                if p == '':
-                    p = '1'
-                print("pow:", p)
-                self.funcs.append(lambda x: float(a) * (x ** p))
+        fn_list = make_list(s)
+        for fn in fn_list:
+            self.funcs.append(make_lambda(fn))
 
     def value(self, x: float):
         ret = 0
-        for a in self.factors[:-1]:
-            ret += a
-            ret *= x
-            print(ret)
-        ret += self.factors[-1]
+        for fn in self.funcs:
+            ret += fn(x)
         return ret
-
-    def _str_to_list(s: str):
-        s = re.sub(' ', '', s)
-        s = re.sub('\+', ' ', s)
-        s = re.sub('-', ' -', s)
-        s = re.sub('\( -', '(-', s)
-        s = re.sub('-x', '-1x', s)
-        s = re.sub('[^\d|^(]x', ' 1x', s)
-        return list(str.split(s))
 
     # def value(self, x: float):
     #     ret = self.factors[0]
