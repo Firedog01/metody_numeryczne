48eb56f043ede3b40a588046a86ab86eebba8417
Firedog01 <jan.rubacha@onet.pl>
2022-03-09 10:11:18 +0100

    zajecia 1

--- zad1/main.py
+++ zad1/main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import bisection
+import matplotlib.pyplot as plt
+import numpy as np
+from Function import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 03B
 
 
-# Press the green button in the gutter to run the script.
+def plot(fun: Function, range_: (float, float)):
+    plot_precision = 100.0
+    fig = plt.figure()
+
+    x_points = []
+    y_points = []
+
+    step = (range_[1] - range_[0]) / plot_precision
+    for x in np.arange(range_[0], range_[1], step):
+        x_points.append(x)
+        y_points.append(fun.value(x))
+
+    # move axis
+    ax = fig.add_subplot(1, 1, 1)
+    if min(x_points) < 0 < max(x_points):
+        ax.spines['left'].set_position(('data', 0))
+    if min(y_points) < 0 < max(y_points):
+        ax.spines['bottom'].set_position(('data', 0))
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    plt.plot(x_points, y_points)
+    plt.show()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pn1 = Function("2 1 -5.4 -18")
+    plot(pn1, (2, 5))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
