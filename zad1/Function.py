bae785c516e47e29d76c1370a2b41927a21970e3
Firedog01 <jan.rubacha@onet.pl>
2022-03-09 10:19:26 +0100

    horner git

--- zad1/Function.py
+++ zad1/Function.py
@@ -4,28 +4,13 @@ class Function:
     factors = list()
 
     def __init__(self, s: str):
-        print(s)
-        # s = re.sub('\^\-?[0-9]', '', s)
-        # s = re.sub('[^\d]x', '1', s)
-        # s = re.sub()
-        #s = re.sub('\*x\^\d+', '', s)
-        #s = re.sub('x(\^\d+|)', '1', s)
-
-        # s = s.replace(x^[])
-
-        # s = s.replace(" ", "")
-        # s = s.replace("x", "")
-        # s = s.replace("^", "")
-        # s = s.replace("+", " ")
-        # s = s.replace("-", " -")
-        # print(s)
         char_list = list(str.split(s))
         self.factors = list(map(float, char_list))
         print(self.factors)
         pass
 
     def value(self, x: float):
-        ret = 0
-        for i in range(len(self.factors)):
-            ret = ret * x + n
+        ret = self.factors[0]
+        for i in range(1, len(self.factors)):
+            ret = ret * x + self.factors[i]
         return ret
