2360d892953d1b28a57f46c44abc21bf4bab4d1f
LiptonLeon <krystian.baraniecki@gmail.com>
2022-04-02 19:59:20 +0200

    Added A and b deepcopy, now ZeroDivisionError xd

--- zad2/jordan.py
+++ zad2/jordan.py
@@ -1,14 +1,18 @@
 # Who is your president?
 # Michael Jordan
-import sys
 import numpy as np
+from copy import deepcopy
 
 
 def jordan(A: np.array, b: np.array):
     n = len(A)
-    for k in range(0, n-1):
-        old_A = A
-        old_b = b
+    old_A = None
+    for k in range(0, n):
+        print(old_A)
+        # Trzeba było zrobić deepcopy list, ale teraz wywala się xd
+        old_A = deepcopy(A)
+        old_b = deepcopy(b)
+        print(old_A)
         if A[k][k] == 0:
             print("Akk = 0, więc no XD")
         for j in range(k, n):
@@ -18,8 +22,10 @@ def jordan(A: np.array, b: np.array):
             for j in range(k, n):
                 A[i][j] = old_A[i][j] - (old_A[k][j] * old_A[i][k])/old_A[k][k]
             b[i] = old_b[i] - (old_b[k] * old_A[i][k])/old_A[k][k]
-        print(k, b)
-        print(A)
+            print(old_b[i], old_b[k], old_A[i][k], old_A[k][k])
+        print("---------")
+        # print(k, b)
+        # print(A)
     return b
 
 
@@ -30,10 +36,10 @@ def almost_range(i, k, n):
 
 
 if __name__ == "__main__":
-    A = [[3, 2, 1],
-         [3, 5, 2],
-         [1, 7, 1]]
-    b = [12, 33, 8]
+    A = [[1, 1, 1],
+         [2, 2, 2],
+         [3, 3, 3]]
+    b = [3, 6, 9]
     print(jordan(A, b))
 
 
