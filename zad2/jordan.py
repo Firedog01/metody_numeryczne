3baa08cdc3b9cc3f82012dfbc1261bc9559a778b
LiptonLeon <krystian.baraniecki@gmail.com>
2022-04-03 22:38:14 +0200

    Chyba działa xd

--- zad2/jordan.py
+++ zad2/jordan.py
@@ -1,5 +1,7 @@
 # Who is your president?
 # Michael Jordan
+import sys
+
 import numpy as np
 from copy import deepcopy
 
@@ -7,12 +9,11 @@ from copy import deepcopy
 def jordan(A: np.array, b: np.array):
     n = len(A)
     old_A = None
-    for k in range(0, n):
+    for k in range(0, n-1):
         print(old_A)
         # Trzeba było zrobić deepcopy list, ale teraz wywala się xd
         old_A = deepcopy(A)
         old_b = deepcopy(b)
-        print(old_A)
         if A[k][k] == 0:
             print("Akk = 0, więc no XD")
         for j in range(k, n):
@@ -25,7 +26,7 @@ def jordan(A: np.array, b: np.array):
             print(old_b[i], old_b[k], old_A[i][k], old_A[k][k])
         print("---------")
         # print(k, b)
-        # print(A)
+        print(A)
     return b
 
 
@@ -34,12 +35,46 @@ def almost_range(i, k, n):
     ret.remove(k)
     return ret
 
+# Chyba to jest to i działa XDDD
+# https://www.pythonpool.com/gaussian-elimination-python/
+def jordan2(n, a, x):
+    for i in range(n):
+        if a[i][i] == 0.0:
+            sys.exit('Divide by zero detected!')
+
+        for j in range(i + 1, n):
+            ratio = a[j][i] / a[i][i]
+
+            for k in range(n + 1):
+                print(i, j, k)
+                a[j][k] = a[j][k] - ratio * a[i][k]
+
+    x[n - 1] = a[n - 1][n] / a[n - 1][n - 1]
+
+    for i in range(n - 2, -1, -1):
+        x[i] = a[i][n]
+
+        for j in range(i + 1, n):
+            x[i] = x[i] - a[i][j] * x[j]
+
+        x[i] = x[i] / a[i][i]
+
+    print('\nThe solution is: ')
+    for i in range(n):
+        print('X%d = %0.2f' % (i, x[i]), end='\t')
+
 
 if __name__ == "__main__":
-    A = [[1, 1, 1],
-         [2, 2, 2],
-         [3, 3, 3]]
-    b = [3, 6, 9]
-    print(jordan(A, b))
+    A = [[3, 2, 1],
+         [3, 5, 2],
+         [1, 7, 1]]
+    x = [12, 33, 8]
+    a = [[3, 2, 1, 12],
+         [3, 5, 2 , 33],
+         [1, 7, 1, 8]]
+
+    jordan2(3, a, x)
+
+    print(jordan(A, x))
 
 
