416abf0d25b0b20ea153fb38a85b5ab55fa2a2e8
LiptonLeon <krystian.baraniecki@gmail.com>
2022-04-05 00:27:52 +0200

    find_main_elem() wip

--- zad2/jordan.py
+++ zad2/jordan.py
@@ -9,7 +9,7 @@ from copy import deepcopy
 def jordan(A: np.array, b: np.array):
     n = len(A)
     old_A = None
-    for k in range(0, n-1):
+    for k in range(0, n - 1):
         print(old_A)
         # Trzeba było zrobić deepcopy list, ale teraz wywala się xd
         old_A = deepcopy(A)
@@ -17,12 +17,12 @@ def jordan(A: np.array, b: np.array):
         if A[k][k] == 0:
             print("Akk = 0, więc no XD")
         for j in range(k, n):
-            A[k][j] = old_A[k][j]/old_A[k][k]
-        b[k] = old_b[k]/old_A[k][k]
+            A[k][j] = old_A[k][j] / old_A[k][k]
+        b[k] = old_b[k] / old_A[k][k]
         for i in almost_range(0, k, n):
             for j in range(k, n):
-                A[i][j] = old_A[i][j] - (old_A[k][j] * old_A[i][k])/old_A[k][k]
-            b[i] = old_b[i] - (old_b[k] * old_A[i][k])/old_A[k][k]
+                A[i][j] = old_A[i][j] - (old_A[k][j] * old_A[i][k]) / old_A[k][k]
+            b[i] = old_b[i] - (old_b[k] * old_A[i][k]) / old_A[k][k]
             print(old_b[i], old_b[k], old_A[i][k], old_A[k][k])
         print("---------")
         # print(k, b)
@@ -34,6 +34,7 @@ def almost_range(i, k, n):
     ret = [*range(i, n)]
     ret.remove(k)
     return ret
+
 
 # Chyba to jest to i działa XDDD
 # https://www.pythonpool.com/gaussian-elimination-python/
@@ -66,28 +67,39 @@ def jordan2(n, a, x):
 
 def jordan3(a, b):
     n = len(b)
-    print(A, b)
     for k in range(0, n):
-        akk = A[k][k]
+        print(k)
+        print(a, b)
+        #a, b = find_main_elem(a, b)
+        print(a, b)
+        akk = a[k][k]
         for j in range(k, n):
             a[k][j] /= akk
         b[k] /= akk
         for i in almost_range(0, k, n):
-            aik = a[i][k] # cache a(k-1)[i][k]
+            aik = a[i][k]  # cache a(k-1)[i][k]
             for j in range(k, n):
-                a[i][j] -= a[k][j] * aik # a[k][j] = a(k-1)[k][j] / a(k-1)[k][k]
-            b[i] -= b[k] * aik
-        print(A, b)
+                a[i][j] -= a[k][j] * aik  # a[k][j] = a(k-1)[k][j] / a(k-1)[k][k]
+            b[i] -= b[k] * aik  # b[k] = b(k-1)[k] / a[k][k]
+        print(a, b)
     return b
 
+
+def find_main_elem(a, b):
+    n = len(b)
+    j = 0
+    while a[n - 1][j] == 0:
+        j += 1
+    for i in range(j, n):
+        if abs(a[i][j]) > abs(a[j][j]):
+            a[[i, j]] = a [[j, i]]
+            b[[i, j]] = b [[j, i]]
+    return a, b
+
+
 if __name__ == "__main__":
-    A = [[3, 2, 1],
-         [3, 5, 2],
-         [1, 7, 1]]
-    x = [12, 33, 8]
-    a = [[3, 2, 1, 12],
-         [3, 5, 2 , 33],
-         [1, 7, 1, 8]]
-    jordan3(A, x)
-
-
+    a = np.array([[2, 1, -2],
+                  [4, -1, 2],
+                  [1, 3, -4]])
+    x = np.array([2, 10, 2])
+    jordan3(a, x)
